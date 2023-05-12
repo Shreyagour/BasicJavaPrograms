39c4d8620677296a98e8303a2e03e8048f23345a
Shreya <shreyagou16@gmail.com>
2023-05-12 16:04:08 +0530

    modification in primenum program

--- primenumber.java
+++ primenumber.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 class primenumber {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
         int num = 29;
     boolean flag = false;
     for (int i = 2; i <= num / 2; ++i) {
@@ -14,5 +18,5 @@ class primenumber {
       System.out.println(num + " is a prime number.");
     else
       System.out.println(num + " is not a prime number.");
-    }
+}
 }
